37956c62f0c12a5ff99abce98e8fd70d81ebb011
subash-Ash <subashsivakumar34@gmail.com>
2024-03-23 00:17:28 +0530

    added counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule],
+  imports: [RouterOutlet,FormsModule,TestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name:string = '';
+  protaganist:string = 'Geralt'
+  counterValue:number=0;
+  display(name:string){
+    alert(name);
+  }
+  increment(){
+    this.counterValue++;
+  }
+  decrement(){
+    this.counterValue--;
+  }
 }
