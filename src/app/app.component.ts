12eac1ab19e8aa02829b304b4d132203c54e8566
subash-Ash <subashsivakumar34@gmail.com>
2024-03-23 03:11:32 +0530

    added if/else class etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { TestComponent } from './test/test.component';
+import { Directive } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,TestComponent],
+  imports: [RouterOutlet,FormsModule,TestComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -14,6 +16,8 @@ export class AppComponent {
   name:string = '';
   protaganist:string = 'Geralt'
   counterValue:number=0;
+  showStarters:boolean=false;
+  starterPokemon:string[] =["bulbasaur","squirtle","charmander"] ;
   display(name:string){
     alert(name);
   }
@@ -22,5 +26,8 @@ export class AppComponent {
   }
   decrement(){
     this.counterValue--;
+  }
+  showStarter(){
+    this.showStarters=true;
   }
 }
